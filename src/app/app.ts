67c034cd07780f4772b79079c407c14c7be4f2bb
sndr <alexsanderbeny3707@gmail.com>
2025-09-20 19:35:33 +0200

    seconde part

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('NasaWebSite');
-}
+
+  datas = [
+          { title: 'Explore the Docs', link: 'https://angular.dev' },
+          { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+          { title: 'Prompt and best practices for AI', link: 'https://angular.dev/ai/develop-with-ai'},
+          { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+          { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+          { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+        ];
+      }
